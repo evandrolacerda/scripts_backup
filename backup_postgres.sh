378e4f010acac1ab3466bd8846d345e451b93cc2
Evandro Pereira de Lacerda <evandro.lacerda@celepar.pr.gov.br>
2022-10-26 18:43:30 -0300

    Alterado nome do arquivo de script de backup do postgres

--- backup_postgres.sh
+++ backup_postgres.sh
@@ -4,5 +4,6 @@ for i in $(psql -q -A -t -c "SELECT datname FROM pg_database where datname not i
 do
 	pg_dump  $i -Z 5 -U postgres > /home/$USER/backup/$i-$NOW.gz
 done
-#find /home/$USER/backup/ -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
-find /home/$USER/backup/ -type f -mmin +5 -name '*.gz' -execdir rm -- '{}' \;
+
+#delete files older than 7 days
+find /home/$USER/backup/ -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
